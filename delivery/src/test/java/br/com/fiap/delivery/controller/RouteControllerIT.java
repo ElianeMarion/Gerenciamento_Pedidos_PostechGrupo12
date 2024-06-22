ab507084cf503be45f5802754fe87c182701f692
Juan Rodríguez <juanpapiro@hotmail.com>
2024-06-22 09:34:17 -0300

    teste integrado com wiremock

--- delivery/src/test/java/br/com/fiap/delivery/controller/RouteControllerIT.java
+++ delivery/src/test/java/br/com/fiap/delivery/controller/RouteControllerIT.java
@@ -1,19 +1,31 @@
 package br.com.fiap.delivery.controller;
 
-import br.com.fiap.delivery.service.RouteService;
+import br.com.fiap.delivery.dto.route.Route;
+import br.com.fiap.delivery.dto.route.RouteResponse;
+import br.com.fiap.delivery.utils.Util;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.containing;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+@WireMockTest(httpPort = 8000)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 class RouteControllerIT {
@@ -24,11 +36,34 @@ class RouteControllerIT {
 
     private static final String PATH = "/delivery/route";
 
+
     @BeforeEach
     void setup() {
         RestAssured.port = port;
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
 
+    @Test
+    void testFindRouteSuccess() throws Exception {
+        WireMock.stubFor(WireMock.post("/directions/v2:computeRoutes")
+                .withHeader("Content-Type", equalTo("application/json"))
+                .willReturn(WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(Util.serialize(Util.Objects.buildRouteResponse()))));
+
+        RouteResponse resp = given()
+                .queryParam("addressOrigin", "rua leblon")
+                .queryParam("addressDestination", "rua Urca")
+                .when()
+                .get(PATH)
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract().body().as(RouteResponse.class);
+
+        assertThat(resp).isNotNull().isInstanceOf(RouteResponse.class);
+        assertThat(resp.getRoutes()).isNotNull()
+                .asList().element(0).isNotNull().isInstanceOf(Route.class);
     }
 
     @Test
