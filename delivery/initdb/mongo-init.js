7dae749f6180a98ae5883aa0eeb405a073f96c86
juanpapiro <juanpapiro@hotmail.com>
2024-07-09 20:33:12 -0300

    readme

--- delivery/initdb/mongo-init.js
+++ delivery/initdb/mongo-init.js
@@ -17,3 +17,90 @@ db.courier.insertMany([
      }
  ]);
 
+ db.createCollection('delivery');
+
+ db.delivery.insertMany([
+    {
+       "order": {
+            "orderID": 1,
+            "customer": {
+                "customerID": 1,
+                "name": "José Soares",
+                "cpf": "70247079090",
+                "phoneNumber": "11999992232"
+            },
+            "purchaseDate": "2024-05-12T09:11:59",
+            "deliveryDate": "2024-05-14T09:23:35",
+            "status": "Aguardando entrega",
+            "orderLines": [
+                {
+                    "orderLineID": 1,
+                    "product": {
+                        "productID": 1,
+                        "name": "Paracetamol",
+                        "description": "Paracetamol 750",
+                        "price": 8.99,
+                        "quantityStock": 10
+                    },
+                    "quantity": 3,
+                    "price": 8.99
+                }
+            ]
+        },
+        "senderAddress": {
+            "addressID": 1,
+            "street": "Rua Teodoro Sampaio",
+            "number": 561,
+            "complement": null,
+            "city": "São Paulo",
+            "state": "SP",
+            "zipCode": "05405000",
+            "subSector": 0
+        },
+        _id:"668d218e1426f7293d51defa",
+        "departureDate": null,
+        "arrivalDate": null
+    },
+    {
+        "order": {
+            "orderID": 2,
+            "customer": {
+                "customerID": 1,
+                "name": "José Soares",
+                "cpf": "70247079090",
+                "phoneNumber": "11999992232"
+            },
+            "purchaseDate": "2024-05-12T09:11:59",
+            "deliveryDate": "2024-05-14T09:23:35",
+            "status": "Aguardando entrega",
+            "orderLines": [
+                {
+                    "orderLineID": 2,
+                    "product": {
+                        "productID": 1,
+                        "name": "Paracetamol",
+                        "description": "Paracetamol 750",
+                        "price": 8.99,
+                        "quantityStock": 7
+                    },
+                    "quantity": 1,
+                    "price": 8.99
+                }
+            ]
+        },
+        "senderAddress": {
+            "addressID": 1,
+            "street": "Rua Teodoro Sampaio",
+            "number": 561,
+            "complement": null,
+            "city": "São Paulo",
+            "state": "SP",
+            "zipCode": "05405000",
+            "subSector": 0
+        },
+        _id:"668d218e1426f7293d51defb",
+        "departureDate": null,
+        "arrivalDate": null
+    }
+ ]);
+
