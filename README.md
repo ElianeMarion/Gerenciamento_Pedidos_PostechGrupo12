98f07c55454f6e96dfef212546cf444f227d8b22
juanpapiro <juanpapiro@hotmail.com>
2024-07-14 10:18:08 -0300

    merge de branchs e docker-compose unificado

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Gerenciamento de Pedidos Postech - Grupo12
 Challenge da Fase 4 sobre Gerenciamento de Pedidos Integrados com Spring e Microsserviços
+
+### Start stop app
+
+Iniciar aplicação com docker-compose na raiz do projeto
+````
+docker-compose -f docker-compose-app.yaml up --build
+````
+
+Finalizar aplicação com docker-compose removendo volumes
+````
+docker-compose -f docker-compose-app.yaml down -v
+````
--- gateway/src/main/java/br/com/ordertech/gateway/ConfigGateway.java
+++ gateway/src/main/java/br/com/ordertech/gateway/ConfigGateway.java
@@ -1,5 +1,6 @@
 package br.com.ordertech.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -7,29 +8,33 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConfigGateway {
+
+    @Value("${URL_BASE}")
+    private String urlBase;
+
     @Bean
     public RouteLocator custom(RouteLocatorBuilder builder) {
          return builder.routes()
                 .route("customer", r -> r.path("/customer/**")
                         .and().not(p -> p.path("/customer/api/**"))
 //                      .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8081"))
+                        .uri(String.format("http://%s:8081", urlBase)))
                 .route("product", r -> r.path("/product/**")
                         .and().not(p -> p.path("/product/api/**"))
 //                      .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8082"))
+                        .uri(String.format("http://%s:8082", urlBase)))
                 .route("order", r -> r.path("/order/**")
                         .and().not(p -> p.path("/order/api/**"))
 //                      .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8083"))
+                        .uri(String.format("http://%s:8083", urlBase)))
                 .route("delivery", r -> r.path("/delivery/**")
                         .and().not(p -> p.path("/delivery/api/**"))
 //                      .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8084"))
+                        .uri(String.format("http://%s:8084", urlBase)))
                 .route("load", r -> r.path("/load/**")
                         .and().not(p -> p.path("/load/api/**"))
 //                      .filters(f -> f.stripPrefix(1))
-                        .uri("http://localhost:8085"))
+                        .uri(String.format("http://%s:8085", urlBase)))
                  .build();
     }
 }
